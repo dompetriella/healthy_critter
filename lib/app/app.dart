6e573f9bda11189c340db802449c5901d4afc4a0
Dominic Petriella <50121548+dompetriella@users.noreply.github.com>
2024-07-09 13:26:55 -0400

    base project ready

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:healthy_critter/theme.dart';
 
-class App extends StatelessWidget {
+class App extends ConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      title: 'App',
+      themeMode: ThemeMode.light,
+      theme: lightTheme,
+    );
   }
 }
